43648787c70d02b9b638e3e8f6121fa15c7fb140
Michael <mnamie20@gmail.com>
2025-12-20 14:38:15 +0100

    Port to Zig

---
+++ src/path.zig
@@ -0,0 +1,80 @@
+const std = @import("std");
+const utils = @import("./utils.zig");
+
+const win = std.os.windows;
+const reg = win.advapi32;
+
+const Allocator = std.mem.Allocator;
+
+/// Path Environment object
+pub const PathEnv = struct {
+    allocator: Allocator,
+    option: utils.Option,
+    path_str: []u8,
+    validation_path_str: []u8,
+    cleaned_path_str: []u8,
+    is_clean: bool = true,
+
+    /// Path Environment initializer
+    pub fn init(allocator: Allocator, option: utils.Option) !PathEnv {
+        const path_str = try utils.fetchPathEnvVariable(allocator);
+        const validation_path_str = try utils.fetchPathEnvVariable(allocator);
+        const cleaned_path_str = try utils.fetchPathEnvVariable(allocator);
+
+        utils.pathToCompatiblePath(path_str);
+        utils.pathToCompatiblePath(validation_path_str);
+        utils.pathToCompatiblePath(cleaned_path_str);
+
+        return PathEnv{ .allocator = allocator, .option = option, .path_str = path_str, .validation_path_str = validation_path_str, .cleaned_path_str = cleaned_path_str };
+    }
+
+    /// Deinitialize owned resources
+    pub fn deinit(self: *PathEnv) void {
+        self.allocator.free(self.path_str);
+        self.allocator.free(self.validation_path_str);
+        self.allocator.free(self.cleaned_path_str);
+    }
+
+    /// Print cleaned path environment variable
+    fn printCleanPath(self: *PathEnv) void {
+        std.debug.print("Path: [\n", .{});
+
+        var iter = std.mem.splitAny(u8, self.path_str, ";");
+        while (iter.next()) |word| {
+            std.debug.print("\t{s}\n", .{word});
+        }
+
+        std.debug.print("]\n", .{});
+    }
+
+    /// Print missing path environment variable targets
+    fn printMissingPathTargets(self: *PathEnv) !void {
+        std.debug.print("Missing path targets: [\n", .{});
+
+        self.is_clean = true;
+
+        var iter = std.mem.splitAny(u8, self.validation_path_str, ";");
+        while (iter.next()) |word| {
+            if (std.mem.containsAtLeast(u8, word, 1, "%")) continue;
+            if (!utils.fileExists(word)) {
+                std.debug.print("\t{s}\n", .{word});
+                self.cleaned_path_str = try std.mem.replaceOwned(u8, self.allocator, self.cleaned_path_str, word, "");
+                self.is_clean = false;
+            }
+        }
+        if (self.is_clean) std.debug.print("\t[*] All path targets exist\n", .{});
+        std.debug.print("]\n", .{});
+    }
+
+    /// Conditionally clean path environment variable, writing to registry, depending on if there were issues
+    fn conditionallyCleanPath(self: *PathEnv) void {
+        if (self.is_clean) return;
+    }
+
+    /// Launch whole process
+    pub fn run(self: *PathEnv) !void {
+        self.printCleanPath();
+        try self.printMissingPathTargets();
+        self.conditionallyCleanPath();
+    }
+};
---
+++ src/utils.zig
@@ -0,0 +1,85 @@
+const std = @import("std");
+const win = std.os.windows;
+const reg = win.advapi32;
+
+const Allocator = std.mem.Allocator;
+
+pub const Option = enum { print, clean };
+
+// Registry key and value we want to pull
+// const key = std.unicode.utf8ToUtf16LeStringLiteral("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment");
+const key = std.unicode.utf8ToUtf16LeStringLiteral("Environment");
+const val = std.unicode.utf8ToUtf16LeStringLiteral("Path");
+
+/// Fetches path environment variable return, converting the winapi utf16le to u8
+pub fn fetchPathEnvVariable(allocator: Allocator) ![]u8 {
+    // Windows HKEY for resulting registry entry
+    var hkey: win.HKEY = undefined;
+
+    const status = reg.RegOpenKeyExW(
+        win.HKEY_CURRENT_USER,
+        key,
+        0,
+        win.KEY_READ,
+        &hkey,
+    );
+
+    // Return error if registry fails to open
+    if (status != 0)
+        return error.OpenFailed;
+
+    // Deferred registry closing
+    defer {
+        _ = reg.RegCloseKey(hkey);
+    }
+
+    // Fetch data length of result, so we can allocate our buffer
+    var data_len: win.DWORD = 0;
+    if (reg.RegQueryValueExW(hkey, val, null, null, null, &data_len) != 0)
+        return error.QuerySizeFailed;
+
+    // Allocate buffer and defer cleanup
+    const buffer = try allocator.alloc(u8, data_len);
+    const buffer_ptr: ?*u8 = @ptrCast(buffer.ptr);
+
+    // Finally query registry key/value
+    if (reg.RegQueryValueExW(hkey, val, null, null, buffer_ptr, &data_len) != 0)
+        return error.QueryFailed;
+
+    // winapi works with utf16le; need to convert to utf8 to work with the rest of Zig stdlib
+    const product_name_utf16: [*]const u16 = @ptrCast(@alignCast(buffer_ptr));
+    const product_name = try std.unicode.utf16LeToUtf8Alloc(allocator, product_name_utf16[0 .. data_len / 2]);
+
+    return product_name;
+}
+
+/// Converts a Windows style path with '\'s into a compatible path with '/'s
+pub fn pathToCompatiblePath(path: []u8) void {
+    std.mem.replaceScalar(u8, path, '\\', '/');
+}
+
+/// Checks if a str exists either as a directory or file
+pub fn fileExists(path: []const u8) bool {
+    _ = std.fs.openFileAbsolute(path, .{}) catch |err| {
+        if (err == error.IsDir) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    return true;
+}
+
+/// Parses command line arguments to our CLI flag options
+pub fn parseArgsToOption(allocator: Allocator) !Option {
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+
+    var option: Option = Option.print;
+
+    while (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, "-c")) option = Option.clean;
+    }
+
+    return option;
+}
