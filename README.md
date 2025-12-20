d06d25e6580f8f66839d8d36d9f8ffb12cc8bc83
Michael <mnamie20@gmail.com>
2025-12-20 22:21:19 +0100

    Revert back to rust version, Zig does not support writing to winreg

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # PathHunter
 
-PathHunter is a quick Windows utility I wrote to check which targets in my Path environment variable no longer exist.
+PathHunter is a quick Windows utility I wrote to check which targets in my Path environment variable no longer exist, and to quickly remove them, keeping my Path clean. Also removes duplicate Path entries, should they exist.
 
 ## Buid instructions:
 
-1. `zig build -Doptimize=ReleaseSmall`
+1. `cargo build --release`
+
+## Usage
+
+```bash
+Usage: ph [FLAGS]
+
+FLAGS:
+        -h      Print usage
+        -b      Print pre-populated Path (default is populated Path)
+        -w      Write cleaned Path to registry (default is display only)
+        -s      Print System Path (default is user Path; requires admin)
+```
---
+++ src/path.rs
@@ -0,0 +1,90 @@
+use crate::reg;
+
+/// PrintType denotes whether the clean or base Path should be printed
+#[derive(Debug, Clone, Copy)]
+pub enum PrintType {
+    Base,
+    Cleaned,
+}
+
+/// PathEnvVar contains the various versions of the Path environment variable
+/// that we want to use for displaying and/or writing.
+#[derive(Debug, Clone)]
+pub struct PathEnvVar {
+    vec: Vec<String>,
+    cleaned_vec: Vec<String>,
+    new_vec: Vec<String>,
+    print_type: PrintType,
+    reg_type: reg::RegistryType,
+}
+
+impl PathEnvVar {
+    /// Create a new instance of PathEnvVar based on PrintType and RegistryType
+    pub fn new(print_type: PrintType, reg_type: reg::RegistryType) -> Self {
+        let path_str = reg::fetch_path_string(reg_type);
+        let path_vec = split_path_string_to_vec(&path_str);
+        let cleaned_vec = clean_path_vec(&path_vec);
+        PathEnvVar {
+            vec: path_vec,
+            cleaned_vec,
+            new_vec: vec![],
+            print_type,
+            reg_type,
+        }
+    }
+
+    /// Print the Path, as dicatated by the configured PrintType
+    pub fn print(&self) {
+        let iter_target = match self.print_type {
+            PrintType::Base => &self.vec,
+            PrintType::Cleaned => &self.cleaned_vec,
+        };
+        println!("\nPath: [");
+        for path in iter_target.iter() {
+            println!(" {}", path);
+        }
+        println!("]");
+    }
+
+    /// Validate the Path for missing path targets
+    pub fn validate(&mut self) {
+        let mut all_clear: bool = true;
+        println!("\nMissing path targets:");
+        for path in self.cleaned_vec.clone() {
+            if std::fs::metadata(&path).is_err() {
+                all_clear = false;
+                println!(" [*] {}", path);
+            } else if !self.new_vec.contains(&path) {
+                self.new_vec.push(path);
+            };
+        }
+        if all_clear {
+            println!(" [*] All clear")
+        }
+    }
+
+    /// After removing dead links and duplicates, write the corrected Path
+    pub fn write_clean_path(&self) {
+        reg::set_path_string(self.reg_type, &self.new_vec.join(";"));
+    }
+}
+
+fn split_path_string_to_vec(path_str: &str) -> Vec<String> {
+    path_str
+        .split(";")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.to_owned())
+        .collect()
+}
+
+fn clean_path_vec(path_vec: &Vec<String>) -> Vec<String> {
+    let mut res: Vec<String> = vec![];
+    for path in path_vec {
+        let mut cleaned_str: String = path.to_owned();
+        for (key, value) in std::env::vars() {
+            cleaned_str = path.replace(&format!("%{}%", key.to_uppercase()), &value);
+        }
+        res.push(cleaned_str);
+    }
+    res
+}
